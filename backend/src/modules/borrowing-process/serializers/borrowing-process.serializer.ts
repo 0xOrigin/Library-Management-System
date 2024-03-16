4e8dcfa54d4820256218fda07bccb6cc2663a3ba
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-03-17 00:12:27 +0200

    Add borrowings serializer

--- backend/src/modules/borrowing-process/serializers/borrowing-process.serializer.ts
+++ backend/src/modules/borrowing-process/serializers/borrowing-process.serializer.ts
@@ -1,3 +1,18 @@
+import { Expose } from "class-transformer";
+
 export class BorrowingProcessSerializer {
+  @Expose()
+  id: string;
+
+  @Expose()
+  book: object;
+
+  @Expose()
+  checkoutAt: Date;
   
+  @Expose()
+  dueDate: Date;
+
+  @Expose()
+  returnedAt: Date;
 }
