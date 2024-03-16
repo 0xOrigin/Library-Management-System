4fabc36ab877f631d1b0f79e199ff193387dc93b
0xOrigin <egyahmed.ezzat120@gmail.com>
2024-03-17 00:07:55 +0200

    Import models in borrowings module

--- backend/src/modules/borrowing-process/borrowing-process.module.ts
+++ backend/src/modules/borrowing-process/borrowing-process.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BorrowingProcessService } from './services/borrowing-process.service';
 import { BorrowingProcessController } from './controllers/borrowing-process.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BorrowingProcess } from './models/borrowing-process.model';
+import { Book } from '../books/models/book.model';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BorrowingProcess, Book])],
   controllers: [BorrowingProcessController],
   providers: [BorrowingProcessService],
 })
